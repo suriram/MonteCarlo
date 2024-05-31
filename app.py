f8b42af73671f175d55777fe38642635917a6274
suriram <mariusfossen@gmail.com>
2024-05-31 14:17:25 +0200

    store filer2

--- app.py
+++ app.py
@@ -33,14 +33,6 @@ du.configure_upload(app, UPLOAD_FOLDER_ROOT)
 app.title = 'Monte Carlo Simulering'
 server = app.server
 
-color_mode_switch =  html.Span(
-    [
-        dbc.Label(className="fa fa-moon", html_for="color-mode-switch"),
-        dbc.Switch( id="color-mode-switch", value=False, className="d-inline-block ms-1", persistence=True),
-        dbc.Label(className="fa fa-sun", html_for="color-mode-switch"),
-    ]
-)
-
 def format_with_space(number):
     return '{:,.0f}'.format(number).replace(',', ' ')
 
